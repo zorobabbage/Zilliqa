455c5c2c00d2ddf3cd9c4c48f1bb2befed342280
chetanphirke <chetan@zilliqa.com>
2020-11-25 14:41:42 +0800

    fix styling

--- src/libNetwork/P2PComm.cpp
+++ src/libNetwork/P2PComm.cpp
@@ -209,7 +209,9 @@ void P2PComm::CloseAndFreeBufferEvent(struct bufferevent* bufev) {
     std::unique_lock<std::mutex> lock(m_mutexPeerConnectionCount);
     if (m_peerConnectionCount[ipAddr] > 0) {
       m_peerConnectionCount[ipAddr]--;
-    LOG_GENERAL(INFO, "Chetan reducing count ipaddr="<<ipAddr<< "m_peerConnectionCount="<< m_peerConnectionCount[ipAddr]);
+      LOG_GENERAL(INFO, "Chetan reducing count ipaddr="
+                            << ipAddr << "m_peerConnectionCount="
+                            << m_peerConnectionCount[ipAddr]);
     }
   }
   bufferevent_free(bufev);
@@ -1254,7 +1256,8 @@ void P2PComm::AcceptConnectionCallback([[gnu::unused]] evconnlistener* listener,
       return;
     }
     m_peerConnectionCount[from.GetIpAddress()]++;
-    LOG_GENERAL(INFO, "Chetan m_peerConnectionCount="<< m_peerConnectionCount[from.GetIpAddress()]);
+    LOG_GENERAL(INFO, "Chetan m_peerConnectionCount="
+                          << m_peerConnectionCount[from.GetIpAddress()]);
   }
 
   // Set up buffer event for this new connection
@@ -1313,7 +1316,8 @@ void P2PComm::AcceptConnectionCallbackForSeed(
       return;
     }
     m_peerConnectionCount[from.GetIpAddress()]++;
-    LOG_GENERAL(INFO, "Chetan m_peerConnectionCount="<< m_peerConnectionCount[from.GetIpAddress()]);
+    LOG_GENERAL(INFO, "Chetan m_peerConnectionCount="
+                          << m_peerConnectionCount[from.GetIpAddress()]);
   }
 
   // Set up buffer event for this new connection
