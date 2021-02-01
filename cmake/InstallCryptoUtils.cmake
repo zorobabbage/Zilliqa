d9ab13bbe84dad9152ade1aad9a836aa253c208f
chetanphirke <chetan@zilliqa.com>
2021-02-01 16:58:19 +0800

    create softlink

--- cmake/InstallCryptoUtils.cmake
+++ cmake/InstallCryptoUtils.cmake
@@ -18,5 +18,50 @@ execute_process(
 )
 
 if(NOT "${CRYPTOUTILS_INSTALL_RET}" STREQUAL "0")
-    message(FATAL_ERROR "Error when downloading cryptoutils (1) repo, see more in log ${CRYPTOUTILS_INSTALL_LOG}")
+    message(FATAL_ERROR "Error when building and installing cryptoutils (1), see more in log ${CRYPTOUTILS_INSTALL_LOG}")
 endif()
+
+
+# generate build directory
+execute_process(
+    COMMAND ${CMAKE_COMMAND}
+        -H${CRYPTOUTILS_SOURCE_DIR}
+        -B${CRYPTOUTILS_BINARY_DIR}
+        -DCMAKE_INSTALL_PREFIX=${CRYPTOUTILS_INSTALL_DIR}
+        -DCMAKE_BUILD_TYPE:STRING=Release
+        -Wno-dev
+    RESULT_VARIABLE CRYPTOUTILS_INSTALL_RET
+    OUTPUT_FILE ${CRYPTOUTILS_INSTALL_LOG}
+    ERROR_FILE ${CRYPTOUTILS_INSTALL_LOG}
+)
+
+if(NOT "${CRYPTOUTILS_INSTALL_RET}" STREQUAL "0")
+    message(FATAL_ERROR "Error when building and installing cryptoutils (2), see more in log ${CRYPTOUTILS_INSTALL_LOG}")
+endif()
+
+# build and install cryptoutils
+execute_process(
+    COMMAND ${CMAKE_COMMAND} --build ${CRYPTOUTILS_BINARY_DIR} -- -j${N}
+    RESULT_VARIABLE CRYPTOUTILS_INSTALL_RET
+    OUTPUT_FILE ${CRYPTOUTILS_INSTALL_LOG}
+    ERROR_FILE ${CRYPTOUTILS_INSTALL_LOG}
+)
+
+if(NOT "${CRYPTOUTILS_INSTALL_RET}" STREQUAL "0")
+    message(FATAL_ERROR "Error when building and installing cryptoutils (3), see more in log ${CRYPTOUTILS_INSTALL_LOG}")
+endif()
+
+execute_process(
+    COMMAND ${CMAKE_COMMAND} --build ${CRYPTOUTILS_BINARY_DIR} --target install
+    RESULT_VARIABLE CRYPTOUTILS_INSTALL_RET
+    OUTPUT_FILE ${CRYPTOUTILS_INSTALL_LOG}
+    ERROR_FILE ${CRYPTOUTILS_INSTALL_LOG}
+)
+
+if(NOT "${CRYPTOUTILS_INSTALL_RET}" STREQUAL "0")
+    message(FATAL_ERROR "Error when building and installing cryptoutils (4), see more in log ${CRYPTOUTILS_INSTALL_LOG}")
+endif()
+
+list(APPEND CMAKE_PREFIX_PATH ${CRYPTOUTILS_INSTALL_DIR})
+list(APPEND CMAKE_MODULE_PATH "${CRYPTOUTILS_INSTALL_DIR}/lib/cmake/cryptoutils")
+link_directories(${CRYPTOUTILS_INSTALL_DIR}/lib)
