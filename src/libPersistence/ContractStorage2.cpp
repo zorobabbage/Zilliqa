81355d04fbde2237f80d6afb8f238a5d07985933
chetanphirke <chetan@zilliqa.com>
2021-04-17 01:00:32 +0800

    update log clear

--- src/libPersistence/ContractStorage2.cpp
+++ src/libPersistence/ContractStorage2.cpp
@@ -1057,7 +1057,7 @@ dev::h256 ContractStorage2::GetContractStateHashCore(const dev::h160& address,
   // iterate the raw protobuf string and hash
   SHA2<HashType::HASH_VARIANT_256> sha2;
   for (const auto& state : states) {
-    if (LOG_SC == false) {
+    if (LOG_SC) {
       LOG_GENERAL(INFO, "state key: "
                             << state.first << " value: "
                             << DataConversion::CharArrayToString(state.second));
@@ -1067,6 +1067,7 @@ dev::h256 ContractStorage2::GetContractStateHashCore(const dev::h160& address,
       sha2.Update(state.second);
     }
   }
+  LOG_GENERAL(INFO, "states count map="<< states.size());
   states.clear();
   startMem = DisplayPhysicalMemoryStats("End GetContractStateHashCore", 0, startMem);
   // return dev::h256(sha2.Finalize());
